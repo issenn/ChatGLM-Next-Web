8aa07b6392a4c603c15ae62bd178bfb674f23e8b
issenn <28648060+issenn@users.noreply.github.com>
2023-06-12 08:40:19 +0800

    fix: fix chat name.

--- app/locales/it.ts
+++ app/locales/it.ts
@@ -11,7 +11,7 @@ const it: LocaleType = {
     ChatItemCount: (count: number) => `${count} messaggi`,
   },
   Chat: {
-    SubTitle: (count: number) => `${count} messaggi con ChatGPT`,
+    SubTitle: (count: number) => `${count} messaggi con ChatGLM`,
     Actions: {
       ChatList: "Vai alla Chat List",
       CompressedHistory: "Prompt di memoria della cronologia compressa",
@@ -41,7 +41,7 @@ const it: LocaleType = {
     Copy: "Copia tutto",
     Download: "Scarica",
     MessageFromYou: "Messaggio da te",
-    MessageFromChatGPT: "Messaggio da ChatGPT",
+    MessageFromChatGPT: "Messaggio da ChatGLM",
   },
   Memory: {
     Title: "Prompt di memoria",
